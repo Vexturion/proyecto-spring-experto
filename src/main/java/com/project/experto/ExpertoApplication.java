8f2e3b83ed6aa02be39123164098131e91942808
Vexturion <horizontedesucesos@outlook.com>
2022-08-25 19:17:28 -0500

    First commit
    iniciamos el proyecto

---
+++ src/main/java/com/project/experto/ExpertoApplication.java
@@ -0,0 +1,13 @@
+package com.project.experto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpertoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/experto/ExpertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.experto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
