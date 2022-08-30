3931525e1e2dd2f1301fd03a3daf4d20d6def916
Vexturion <horizontedesucesos@outlook.com>
2022-08-30 01:02:20 -0500

    Se agregan funciones como listar usuarios con thymeleaf Se inserta usuario directamente como ejemplo

--- src/main/java/com/project/experto/controllers/IndexController.java
+++ src/main/java/com/project/experto/controllers/IndexController.java
@@ -1,13 +1,44 @@
 package com.project.experto.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.experto.models.Usuario;
 
 @Controller
+@RequestMapping(value = "/app")
 public class IndexController {
     
-    @GetMapping({"/index","/","home"})
-    public String index(){
+    @GetMapping({"/index","/home"})
+    public String index(Model model){
+        model.addAttribute("titulo", "hola titulo!");
         return "index";
     }
+
+    @GetMapping(value = "/perfil")
+        String perfil (Model model){
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Pepe");
+            usuario.setApellido("Lopez");
+            usuario.setEmail("usuario@correo.com");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
+    }
+
+    @GetMapping(value = "/listar")
+    String listar (Model model){
+        List<Usuario> usuarios = new ArrayList<>();
+        model.addAttribute("titulo", "Listado de usuarios");
+        model.addAttribute("usuarios", usuarios);
+
+        return "listar";
+    }
+    
 }
---
+++ src/main/java/com/project/experto/models/Usuario.java
@@ -0,0 +1,12 @@
+package com.project.experto.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String email;
+}
