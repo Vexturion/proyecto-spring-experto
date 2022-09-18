f643f288f32fc779be01b9c1857e5610fd4df685
Vexturion <horizontedesucesos@outlook.com>
2022-09-18 01:11:09 -0500

    Se agregan formularios y el controlador que los maneja

--- src/main/java/com/project/experto/controllers/FormController.java
+++ src/main/java/com/project/experto/controllers/FormController.java
@@ -1,21 +1,31 @@
 package com.project.experto.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/formularios")
 public class FormController {
     
     @GetMapping("/form")
-    public String getForm(){
+    public String getForm(Model model ){
         return "formularios/form";
     }
 
     @PostMapping("/form")
-    public String postForm(){
+    public String postForm(Model model, @RequestParam String username,
+    @RequestParam String password,
+    @RequestParam String email){
+
+        model.addAttribute("titulo", "resultado del formulario");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
+
         return "formularios/resultado";
     }
 
