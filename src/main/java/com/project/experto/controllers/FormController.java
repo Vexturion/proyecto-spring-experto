3bcc65d57b422fce01e24cf56109390e8f6131ba
Vexturion <horizontedesucesos@outlook.com>
2022-09-18 15:44:08 -0500

    Se agregan los campos nombre y apellido, queda pendiente su inclusion en el formularios. Se incluyen errores para cuando se envian vacios los campos y se mejoraron con thymeleaf y Spring

--- src/main/java/com/project/experto/controllers/FormController.java
+++ src/main/java/com/project/experto/controllers/FormController.java
@@ -1,34 +1,66 @@
 package com.project.experto.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.project.experto.models.domain.Usuario;
 
 @Controller
 @RequestMapping("/formularios")
 public class FormController {
-    
+
     @GetMapping("/form")
-    public String getForm(Model model ){
+    public String getForm(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("titulo", "Formulario de usuarios");
+        model.addAttribute("usuario", usuario);
         return "formularios/form";
     }
 
     @PostMapping("/form")
-    public String postForm(Model model, @RequestParam String username,
-    @RequestParam String password,
-    @RequestParam String email){
+    public String postForm(@Valid Usuario usuario, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            /* Map<String, String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(), "El campo ".concat(err.getField().concat(" ").concat(err.getDefaultMessage())));
+            });
+            model.addAttribute("error", errores); */
+            return "formularios/form";
+        }
 
         model.addAttribute("titulo", "resultado del formulario");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("usuario", usuario);
 
         return "formularios/resultado";
     }
 
-
+    /*
+     * @PostMapping("/form")
+     * public String postForm(Model model, @RequestParam String username,
+     * 
+     * @RequestParam String password,
+     * 
+     * @RequestParam String email){
+     * 
+     * Usuario usuario = new Usuario();
+     * usuario.setUsername(username);
+     * usuario.setPassword(password);
+     * usuario.setEmail(email);
+     * 
+     * model.addAttribute("titulo", "resultado del formulario");
+     * model.addAttribute("usuario", usuario);
+     * 
+     * return "formularios/resultado";
+     */
+    // }
 
 }
---
+++ src/main/java/com/project/experto/models/domain/Usuario.java
@@ -0,0 +1,64 @@
+package com.project.experto.models.domain;
+
+import javax.validation.constraints.NotEmpty;
+
+public class Usuario {
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String nombre;
+    
+    @NotEmpty
+    private String apellido;
+
+    /**
+     * @return String return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return String return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return String return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
